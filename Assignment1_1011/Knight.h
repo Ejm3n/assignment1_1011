24cd7d9bd7772696e498ede4c3a99df07b81a0d2
NotAdamJabbar <Adam.alabduljabbar@gmail.com>
2024-03-10 17:55:50 -0400

    Update Knight.h

--- Assignment1_1011/Knight.h
+++ Assignment1_1011/Knight.h
@@ -4,6 +4,7 @@
 class Knight : public Player {
 public:
     Knight(const std::string& name);
+    static const std::string specialAttackName;
     void NormalAttack() override;
     void SpecialAttack() override;
 };
